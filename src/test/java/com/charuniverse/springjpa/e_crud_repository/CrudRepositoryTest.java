3356a1a2e326783f9907a214813f28a22cf15432
ahps <akbarputrasrg@gmail.com>
2024-05-31 20:49:51 +0700

    course e: crud repository

---
+++ src/test/java/com/charuniverse/springjpa/e_crud_repository/CrudRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.charuniverse.springjpa.e_crud_repository;
+
+import com.charuniverse.springjpa.entity.Category;
+import com.charuniverse.springjpa.repository.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CrudRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    void testCreate() {
+        Category category = new Category();
+        category.setName("Electronics");
+
+        categoryRepository.save(category);
+        Assertions.assertNotNull(category.getId());
+    }
+
+    @Test
+    void testUpdate() {
+        Category category = categoryRepository.findById(1L).orElse(null);
+        Assertions.assertNotNull(category);
+
+        category.setName("Electronics Terbaru");
+        categoryRepository.save(category);
+
+        category = categoryRepository.findById(1L).orElse(null);
+        Assertions.assertNotNull(category);
+        Assertions.assertEquals("Electronics Terbaru", category.getName());
+    }
+}
