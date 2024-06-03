e92d21c39153aaef8fede9695a3e711862f6c256
ahps <akbarputrasrg@gmail.com>
2024-06-03 10:52:19 +0700

    course l: page result

--- src/main/java/com/charuniverse/springjpa/repository/ProductRepository.java
+++ src/main/java/com/charuniverse/springjpa/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.charuniverse.springjpa.repository;
 
 import com.charuniverse.springjpa.entity.Product;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,6 +16,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findAllByCategory_Name(String name, Sort sort);
 
-    List<Product> findAllByCategory_Name(String name, Pageable pageable);
+    Page<Product> findAllByCategory_Name(String name, Pageable pageable);
 
 }
--- src/test/java/com/charuniverse/springjpa/l_page_result/PageResultTest.java
+++ src/test/java/com/charuniverse/springjpa/l_page_result/PageResultTest.java
@@ -1,4 +1,4 @@
-package com.charuniverse.springjpa.k_paging;
+package com.charuniverse.springjpa.l_page_result;
 
 import com.charuniverse.springjpa.entity.Product;
 import com.charuniverse.springjpa.repository.ProductRepository;
@@ -12,26 +12,34 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 @SpringBootTest
-public class PagingTest {
+public class PageResultTest {
 
     @Autowired
     private ProductRepository productRepository;
 
     @Test
     void findProductsPaging() {
-        Pageable pageable = PageRequest.of(0, 1, Sort.by("id").ascending());
+        Pageable pageable = PageRequest.of(0, 1, Sort.by("id").descending());
         Page<Product> products = productRepository.findAllByCategory_Name("Electronics", pageable);
 
         Assertions.assertEquals(1, products.getContent().size());
-        Assertions.assertEquals("Apple iPhone 14 Pro Max", products.getContent().get(0).getName());
+        Assertions.assertEquals(0, products.getNumber());
+        Assertions.assertEquals(2, products.getTotalElements());
+        Assertions.assertEquals(2, products.getTotalPages());
+
+        Assertions.assertEquals("Apple iPhone 13 Pro Max", products.getContent().get(0).getName());
     }
 
     @Test
     void findProductsPaging2() {
-        Pageable pageable = PageRequest.of(1, 1, Sort.by("id").ascending());
+        Pageable pageable = PageRequest.of(1, 1, Sort.by("id").descending());
         Page<Product> products = productRepository.findAllByCategory_Name("Electronics", pageable);
 
         Assertions.assertEquals(1, products.getContent().size());
-        Assertions.assertEquals("Apple iPhone 13 Pro Max", products.getContent().get(0).getName());
+        Assertions.assertEquals(1, products.getNumber());
+        Assertions.assertEquals(2, products.getTotalElements());
+        Assertions.assertEquals(2, products.getTotalPages());
+
+        Assertions.assertEquals("Apple iPhone 14 Pro Max", products.getContent().get(0).getName());
     }
 }
